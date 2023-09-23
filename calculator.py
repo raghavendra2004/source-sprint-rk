4fe7699df96a16eae0e3187a36e91d1d5668afd3
raghavendra2004 <145823327+raghavendra2004@users.noreply.github.com>
2023-09-23 15:04:48 +0530

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -2,6 +2,8 @@ x = eval(input ("enter the first number:"))
 y = eval(input("enter the second number:"))
 sum = x+y
 diff = x-y
+mul = x*y
 print(sum)
 print(diff)
+print(mul)
 print("this is an calculator")
